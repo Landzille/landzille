f754f6e8d254ffddd350f310ce8f3cd149003b1c
Oyeyemi <mac@Oyeyemi-Korede.local>
2026-02-12 17:25:54 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import DownloadMagazine from "@/components/downloadMagazine";
+// import DownloadMagazine from "@/components/downloadMagazine";
 import Features from "@/components/features";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
@@ -7,7 +7,7 @@ import Holdings from "@/components/holdings";
 import InsightsSection from "@/components/insights/insightSection";
 import OtherHeroSection from "@/components/otherHeros";
 import PartnersSection from "@/components/partnerSection/section";
-import Popup from "@/components/popup";
+// import Popup from "@/components/popup";
 import Reviews from "@/components/review";
 import Stats from "@/components/statsSection";
 import WhyInvest from "@/components/whyInvest";
@@ -17,9 +17,9 @@ const HomePage = () => {
   return (
     <div>
       <Header />
-      <Popup>
+      {/* <Popup>
         <DownloadMagazine />
-      </Popup>
+      </Popup> */}
       <Hero />
       <Features />
       <Stats />

ceda99942b5c500d9f8460f7c525c37002d90e14
Korede <oyeyemikorede5@gmail.com>
2026-03-20 22:06:27 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Instrument_Sans, Lora } from "next/font/google";
+import Image from "next/image";
 import Script from "next/script";
 import SessionProvider from "@/components/sessionProvider";
 import "./globals.css";
@@ -60,6 +61,31 @@ export default function RootLayout({
           src="https://smartarget.online/loader.js?u=fa22648526c52fb6c791519e6488d69c9d1f2e0a"
           strategy="afterInteractive"
         />
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1251654063162775');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1251654063162775&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );

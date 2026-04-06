1bb863abfbe8608507b4aa920e1e0a7cbd1fc463
Korede <oyeyemikorede5@gmail.com>
2026-04-06 22:56:41 +0100

    update

--- utils/resources.tsx
+++ utils/resources.tsx
@@ -9,41 +9,48 @@ interface Resource {
 export const allResources: Resource[] = [
   {
     id: "1",
+    title: "The Healthcare AI Playbook",
+    subtitle: "A Pratical Guide to AI & Robotics Governance",
+    image: "/assets/healthcare-ai-playbook.jpeg",
+    downloadUrl: "/downloads/resources/Healthcare_AI_Playbook.pdf",
+  },
+  {
+    id: "2",
     title: "ROXTON TEXAS",
     subtitle: "Top 20 Reasons to Invest",
     image: "/assets/resources/roxton-texas.jpg",
     downloadUrl: "/downloads/resources/roxton-texas-1.pdf",
   },
   {
-    id: "2",
+    id: "3",
     title: "LEONARD TEXAS",
     subtitle: "The Next Investment Frontier in North Texas",
     image: "/assets/resources/leonard-texas.jpeg",
     downloadUrl: "/downloads/resources/leonard-texas.pdf",
   },
   {
-    id: "3",
+    id: "4",
     title: "I AM MUENSTER",
     subtitle: "Story of a hidden Cooke county Gem",
     image: "/assets/resources/menuster.jpeg",
     downloadUrl: "/downloads/resources/i-am-muenster.pdf",
   },
   {
-    id: "4",
+    id: "5",
     title: "THE NORTH TEXAS GROWTH ENGINE",
     subtitle: "Download the ebook below",
     image: "/assets/resources/north-texas.jpeg",
     downloadUrl: "/downloads/resources/north-texas-growth-engine.pdf",
   },
   {
-    id: "5",
+    id: "6",
     title: "20 TOP REASONS TO INVEST IN GAINESVILLE, TEXAS",
     subtitle: "Download the ebook below",
     image: "/assets/resources/gainesville.jpeg",
     downloadUrl: "/downloads/resources/reasons-to-invest-in-gainesville.pdf",
   },
   {
-    id: "6",
+    id: "7",
     title:
       "TOP 50 LIST OF THE BIGGEST INFRASTRUCTURE EXPANSIONS POWERING DEVELOPMENT ACROSS NORTH TEXAS",
     subtitle: "Download the ebook below",
@@ -51,14 +58,14 @@ export const allResources: Resource[] = [
     downloadUrl: "/downloads/resources/top-50-infrastructure-expansions.pdf",
   },
   {
-    id: "7",
+    id: "8",
     title: "1-MONTH INTERNSHIP PLAN",
     subtitle: "Download the ebook below",
     image: "/assets/resources/internship.jpeg",
     downloadUrl: "/downloads/resources/1-month-internship-plan.pdf",
   },
   {
-    id: "8",
+    id: "9",
     title: "TOP REASONS TO INVEST IN LAND IN TOP CITIES",
     subtitle: "Download the ebook below",
     image: "/assets/resources/invest-noe.jpg",
@@ -66,7 +73,7 @@ export const allResources: Resource[] = [
       "/downloads/resources/top-reasons-to-invest-in-land-in-top-cities.pdf",
   },
   {
-    id: "9",
+    id: "10",
     title: "COLLEGES AS CATALYSTS FOR LAND DEVELOPMENT IN THE U.S",
     subtitle: "Download the ebook below",
     image: "/assets/resources/colleges.jpg",
@@ -74,14 +81,14 @@ export const allResources: Resource[] = [
       "/downloads/resources/colleges-as-catalysts-for-land-development.pdf",
   },
   {
-    id: "10",
+    id: "11",
     title: "STRATEGIC LAND LEASE PLAYBOOK",
     subtitle: "Download the ebook below",
     image: "/assets/resources/strategy.jpg",
     downloadUrl: "/downloads/resources/strategic-land-lease-playbook.pdf",
   },
   {
-    id: "11",
+    id: "12",
     title: "THE IMPACT OF AIRPORT ON ECONOMIC DEVELOPMENT",
     subtitle: "Download the ebook below",
     image: "/assets/resources/impact.jpg",
@@ -89,7 +96,7 @@ export const allResources: Resource[] = [
       "/downloads/resources/the-impact-of-airports-on-economic-development.pdf",
   },
   {
-    id: "12",
+    id: "13",
     title: "THE INFRASTRUCTURE INVESTMENT AND JOBS ACT",
     subtitle: "Download the ebook below",
     image: "/assets/resources/infrastructure.jpg",
@@ -97,7 +104,7 @@ export const allResources: Resource[] = [
       "/downloads/resources/the-infrastructure-investment-and-jobs-act.pdf",
   },
   {
-    id: "13",
+    id: "14",
     title: "THE ROAD TO PROGRESS INFRASTRUCTURE DEVELOPMENT IN THE USA",
     subtitle: "Download the ebook below",
     image: "/assets/resources/road.jpg",
@@ -105,7 +112,7 @@ export const allResources: Resource[] = [
       "/downloads/resources/the-road-to-progress-infrastructure-development.pdf",
   },
   {
-    id: "14",
+    id: "15",
     title: "THE IMPACT OF FARMING ON ECONOMIC DEVELOPMENT IN NORTH TEXAS",
     subtitle: "Download the ebook below",
     image: "/assets/resources/impact-farm.jpg",
@@ -113,7 +120,7 @@ export const allResources: Resource[] = [
       "/downloads/resources/the-impact-of-farming-on-economic-development-in-north-texas.pdf",
   },
   {
-    id: "15",
+    id: "16",
     title: "THE IMPACT OF LAKES ON REAL ESTATE DEVELOPMENT IN THE USA",
     subtitle: "Download the ebook below",
     image: "/assets/resources/impact-lakes.jpeg",
@@ -121,7 +128,7 @@ export const allResources: Resource[] = [
       "/downloads/resources/the-impact-of-lakes-on-real-estate-development-in-the-usa.pdf",
   },
   {
-    id: "16",
+    id: "17",
     title:
       "THE IMPACT OF SEMICONDUCTOR ON REAL ESTATE DEVELOPMENT IN THE UNITED STATES",
     subtitle: "Download the ebook below",
@@ -130,7 +137,7 @@ export const allResources: Resource[] = [
       "/downloads/resources/the-impact-of-semiconductor-on-real-estate-development-in-the-united-states.pdf",
   },
   {
-    id: "17",
+    id: "18",
     title: "LAND INVESTMENT 101",
     subtitle: "Download the Foundations of Successful Land Investment ebook",
     image: "/assets/resources/investment-101.jpg",
